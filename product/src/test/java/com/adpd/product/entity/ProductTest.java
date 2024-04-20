a1f09e6f3e5d4e7bad78e9c00571a83d2945e7fa
Adriano Diaz <adrianodpdiaz@gmail.com>
2024-04-21 00:47:14 +0200

    create dev profile properties

--- product/src/test/java/com/adpd/product/entity/ProductTest.java
+++ product/src/test/java/com/adpd/product/entity/ProductTest.java
@@ -5,16 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(MockitoExtension.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith(MockitoExtension.class)
 class ProductTest {
 
     @Autowired
